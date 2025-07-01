0095e5c5737ad018d5b226d686043a1329c1441f
SirCoutin <58868790+SirCoutin@users.noreply.github.com>
2025-07-01 15:26:44 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,14 +2,17 @@ from PIL import Image
 from collections import Counter
 
 # Function to compare colors with tolerance
-def is_color_in_range(rgb, target_rgb, tolerance=40):
-    return all(abs(c1 - c2) <= tolerance for c1, c2 in zip(rgb, target_rgb))
+def is_color_in_range(rgb, target, tolerance=40):
+    for colors in target:
+        if all(abs(c1 - c2) <= tolerance for c1, c2 in zip(rgb, colors)):
+            return true
+        else:
+            return false
 
 # Function to process image and filter for target color
 def filter_image_by_color(image_path, target_color_name, tolerance=40):
     img = Image.open(image_path).convert("RGB")
     pixels = list(img.getdata())
-    width, height = img.size
     target_rgb = color_db[target_color_name]
 
     matching_pixels = []
