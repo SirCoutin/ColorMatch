12fde552c0f2b3cd177879c5f5fd5ef126a0d101
SirCoutin <58868790+SirCoutin@users.noreply.github.com>
2025-07-03 17:06:08 -0300

    Update coloradd.py
    Added check for name color

--- coloradd.py
+++ coloradd.py
@@ -3,14 +3,14 @@ import os
 from collections import Counter
 from PIL import Image
 
-michel_colors = []
+michel_colors = {}
 
 def add_color_to_michel(image_path, name):
   img = Image.open(image_path).convert("RGB")
   pixels = list(img.getdata())
-  
+
   most_common_match = Counter(pixels).most_common(1)[0][0]
-  michel_colors.append((name, most_common_match))
+  michel_colors[name] = most_common_match
 
 if os.path.exists("michel_colors.json"):
   with open("michel_colors.json", "r") as colors:
@@ -20,11 +20,14 @@ else:
   with open("michel_colors.json", "w") as colors:
             json.dump(michel_colors, colors)
 
-image_path = input("Enter the path of the image you want to save")
-color_name = input("Enter the name of the respective Michel color")
+image_path = input("Enter the path of the image you want to save: ")
+color_name = input("Enter the name of the respective Michel color: ").lower()
 
-add_color_to_michel(image_path, color_name.lower())
+if color_name in michel_colors:
+     print("This color name is already in use!")
+     exit(0)
+else:
+    add_color_to_michel(image_path, color_name)
 
 with open("michel_colors.json", "w") as colors:
   json.dump(michel_colors, colors)
-
